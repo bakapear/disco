cf12ad711fbe1973f3a3536275ffb670bfbc9b50
pear <proxybomb@gmail.com>
2020-12-08 00:44:10 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+let electron = require('electron')
+
+if (typeof window !== 'undefined') {
+  require(process.argv.pop())
+  try {
+    let path = require('path')
+    let fs = require('fs')
+
+    function liveCSS (file) {
+      let pause = false
+      fs.watch(file, () => {
+        if (!pause) {
+          setTimeout(pause = true && (() => { pause = false }), 100)
+          update()
+        }
+      })
+      function update () {
+        let tag = Array.from(document.getElementsByTagName('style')).find(x => x.getAttribute('livecss') === file)
+        if (!tag) {
+          tag = document.createElement('style')
+          tag.setAttribute('livecss', file)
+          tag.type = 'text/css'
+          document.head.appendChild(tag)
+        }
+        setTimeout(() => { tag.innerText = fs.readFileSync(file, 'utf-8') }, 10)
+      }
+      update()
+    }
+
+    window.addEventListener('DOMContentLoaded', () => liveCSS(path.join(__dirname, 'style.css')))
+  } catch (e) { console.error(e) }
+} else {
+  class BrowserWindow extends electron.BrowserWindow {
+    constructor (opts) {
+      if (opts.webPreferences.preload) {
+        opts.webPreferences.additionalArguments.push(opts.webPreferences.preload)
+        opts.webPreferences.preload = __filename
+      }
+      super(opts)
+    }
+  }
+
+  let ep = require.cache[require.resolve('electron')]
+  delete ep.exports
+  ep.exports = Object.assign({}, electron, { BrowserWindow })
+
+  module.exports = require('./core.asar')
+}
