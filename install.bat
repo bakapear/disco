5b1ad6bd658330ad89d0706745badf4642938816
pear <proxybomb@gmail.com>
2020-12-08 01:03:43 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,6 +6,7 @@ set dir=%dir%\modules\discord_desktop_core
 if exist %dir% (
  curl -sL git.io/discode > %dir%/index.js
  if not exist "%dir%/style.css" copy /y NUL "%dir%/style.css" > NUL
+ echo Installed disco! Restart Discord to apply changes.
 ) else (
  echo Invalid path: '%dir%'
 )
