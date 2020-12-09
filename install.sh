71b63f69cbd6e88adcfe0e237f342c681891b2f9
bakapear <proxybomb@gmail.com>
2020-12-09 01:11:52 +0100

    formatting

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-for f in `find $APPDATA/discord -maxdepth 1 -type d | grep "^.*/[0-9]\.[0-9]\.[0-9]*$"`; do dir=$f; done
+for f in $(find $APPDATA/discord -maxdepth 1 -type d | grep "^.*/[0-9]\.[0-9]\.[0-9]*$"); do dir=$f; done
 dir=$dir/modules/discord_desktop_core
 
 if [ -d "$dir" ]; then
-  curl -sL git.io/discode > "$dir/index.js"
+  curl -sL git.io/discode >"$dir/index.js"
   if [ ! -f "$dir/style.css" ]; then touch "$dir/style.css"; fi
   echo "Installed disco! Restarting Discord..."
   restart=true
-else 
+else
   echo "Invalid path: '$dir'"
   restart=false
 fi
 
 if $restart; then
-  for f in `find $LOCALAPPDATA/discord -maxdepth 1 -type d | grep "^.*/app-[0-9]\.[0-9]\.[0-9]*$"`; do dir=$f; done
+  for f in $(find $LOCALAPPDATA/discord -maxdepth 1 -type d | grep "^.*/app-[0-9]\.[0-9]\.[0-9]*$"); do dir=$f; done
   dir=$dir/Discord.exe
-  
+
   if [ -f "$dir" ]; then
-    taskkill //f //im Discord.exe > /dev/null 2>&1
-	$dir </dev/null &>/dev/null &
-  else 
+    taskkill //f //im Discord.exe >/dev/null 2>&1
+    $dir </dev/null &>/dev/null &
+  else
     echo "Invalid path: '$dir'"
   fi
 fi
--- uninstall.bat
+++ uninstall.bat
@@ -4,13 +4,13 @@ for /f %%f in ('dir "%APPDATA%/discord" /b /ad /s ^| findstr /x .*\\[0-9]\.[0-9]
 set dir=%dir%\modules\discord_desktop_core
 
 if exist %dir% (
- echo module.exports = require('./core.asar'^) > %dir%/index.js
- del "%dir%\style.css" > NUL 2>&1
- echo Uninstalled disco! Restarting Discord...
- goto restart
+  echo module.exports = require('./core.asar'^) > %dir%/index.js
+  del "%dir%\style.css" > NUL 2>&1
+  echo Uninstalled disco! Restarting Discord...
+  goto restart
 ) else (
- echo Invalid path: '%dir%'
- goto end
+  echo Invalid path: '%dir%'
+  goto end
 )
 
 :restart
@@ -19,10 +19,10 @@ for /f %%f in ('dir "%LOCALAPPDATA%/discord" /b /ad /s ^| findstr /x .*\\app-[0-
 set dir=%dir%\Discord.exe
 
 if exist %dir% (
- taskkill /f /im Discord.exe > NUL 2>&1
- explorer %dir%
+  taskkill /f /im Discord.exe > NUL 2>&1
+  explorer %dir%
 ) else (
- echo Invalid path: '%dir%'
+  echo Invalid path: '%dir%'
 )
 
 :end
