e0c0d2ad9fefcc83923c786d1e6f1345fbc09a82
bakapear <proxybomb@gmail.com>
2022-09-02 12:51:11 +0200

    move injection to resources folder

--- install.sh
+++ install.sh
@@ -2,25 +2,29 @@
 
 for f in $(find $LOCALAPPDATA/discord -maxdepth 1 -type d | grep "app-"); do ver=$f; done
 
-dir=$f/modules/discord_desktop_core-2/discord_desktop_core
-app=$f/Discord.exe
+dir=$f/resources/app
+app=$f/discord.exe
 
-if [ ! -d "$dir" ]; then
-  echo "Invalid path: '$dir'"
-  exit
-elif [ ! -f "$app" ]; then
-  echo "Invalid path: '$app'"
-  exit
-elif [[ $(head -n 1 "$dir/index.js") == "/* disco */" ]]; then
+disco=$dir/disco.js
+pack=$dir/package.json
+
+if [ -f "$disco" ]; then
   echo "disco is already installed."
   exit
 fi
 
-curl -sL git.io/discode >"$dir/index.js"
+if [ ! -d "$dir" ]; then mkdir $dir; fi
+
+curl -sL git.io/discode > "$disco"
+echo '{ "main": "disco.js", "name": "discord", "private": true }' > "$pack"
 
 echo "-- disco successfully installed! --"
-echo "Restarting Discord..."
 
-taskkill //f //im Discord.exe >/dev/null 2>&1
-"$app" </dev/null &>/dev/null &
-exit
+if tasklist -fi "ImageName eq discord.exe" -fo csv | grep -i "discord.exe" > /dev/null
+then
+  echo "Restarting Discord..."
+
+  taskkill -f -im discord.exe > /dev/null
+  "$app" </dev/null &>/dev/null &
+  exit
+fi
--- uninstall.sh
+++ uninstall.sh
@@ -2,25 +2,30 @@
 
 for f in $(find $LOCALAPPDATA/discord -maxdepth 1 -type d | grep "app-"); do ver=$f; done
 
-dir=$f/modules/discord_desktop_core-2/discord_desktop_core
-app=$f/Discord.exe
+dir=$f/resources/app
+app=$f/discord.exe
 
-if [ ! -d "$dir" ]; then
-  echo "Invalid path: '$dir'"
-  exit
-elif [ ! -f "$app" ]; then
-  echo "Invalid path: '$app'"
-  exit
-elif [[ $(head -n 1 "$dir/index.js") != "/* disco */" ]]; then
+disco=$dir/disco.js
+pack=$dir/package.json
+
+if [ ! -f "$disco" ]; then
   echo "disco is not installed."
   exit
 fi
 
-echo "module.exports = require('./core.asar')" >"$dir/index.js"
+rm "$disco"
+
+if grep -q "disco.js" "$pack"; then rm "$pack"; fi
+
+rmdir "$dir" > /dev/null
 
 echo "-- disco successfully uninstalled! --"
-echo "Restarting Discord..."
 
-taskkill //f //im Discord.exe >/dev/null 2>&1
-"$app" </dev/null &>/dev/null &
-exit
+if tasklist -fi "ImageName eq discord.exe" -fo csv | grep -i "discord.exe" > /dev/null
+then
+  echo "Restarting Discord..."
+
+  taskkill -f -im discord.exe > /dev/null
+  "$app" </dev/null &>/dev/null &
+  exit
+fi
